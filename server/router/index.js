282beae87473629bb4a8c579c98669087358da0e
Brian Fajardo <bmf5@hotmail.com>
2017-10-04 23:40:51 -0400

    Fetch summoner, most recent ranked matches and their gameId's

--- server/router/index.js
+++ server/router/index.js
@@ -1,3 +1,6 @@
+const axios = require('axios')
+const { RIOT_API_KEY } = require('../../keys')
+
 const router = app => {
   // Developer message
   app.get('/api', (req, res) => {
@@ -5,8 +8,25 @@ const router = app => {
   })
 
   // Fetch users' warding stats in the 20 most recent ranked matches.
-  app.post('/api/wardstats', (req, res) => {
-    res.send({ message: 'Ward stat route' }).status(200)
+  app.post('/api/wardstats', async (req, res) => {
+    let stats // summoner, profile pic
+    const { summonerName, region } = req.body
+    const URL = `https://${region}.api.riotgames.com/lol`
+    const summonerURL = `${URL}/summoner/v3/summoners/by-name/${summonerName}?api_key=${RIOT_API_KEY}`
+
+    if (!summonerName || !region) {
+      res
+        .send({ error: 'Please provide a summoner name and region.' })
+        .status(400)
+    }
+
+    // Retrieve accountId by summoner name.
+    const summoner = await axios.get(summonerURL).then(({ data }) => data)
+
+    // Retrieve matchlist for ranked games played on given accountId.
+    const matchListsURL = `${URL}/match/v3/matchlists/by-account/${summoner.accountId}?endIndex=20&api_key=${RIOT_API_KEY}`
+    const { matches } = await axios.get(matchListsURL).then(({ data }) => data)
+    const gameIdArray = matches.map(({ gameId }) => gameId)
   })
 }
 
