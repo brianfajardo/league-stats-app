44369dcccce7dea5f09a6346cea646db184fea40
Brian Fajardo <bmf5@hotmail.com>
2017-10-05 17:21:01 -0400

    Add a cleaning build step. Create Heroku Procfile.

--- keys/dev.js
+++ keys/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  RIOT_API_KEY: 'RGAPI-10d2e405-9796-4edb-a41c-3d8a3092c636',
+  RIOT_API_KEY: 'RGAPI-8a9d70f9-c99d-4546-add8-bba9e553529d',
   SERVER_URL: 'http://localhost:8000/api/wardstats'
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: ['babel-polyfill', './client/index.js'],
@@ -30,5 +31,10 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
 }
